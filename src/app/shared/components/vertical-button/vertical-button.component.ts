48d40512b9e0d59aee4163cd145b8f4ae791b23f
devcobos <devcobos@gmail.com>
2025-02-14 21:19:33 +0100

    change buttons

--- src/app/shared/components/vertical-button/vertical-button.component.ts
+++ src/app/shared/components/vertical-button/vertical-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,11 +11,5 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class VerticalButtonComponent {
   @Input({ required: true }) icon!: string;
-  @Input({ required: true }) label!: string;
-
-  @Output() click = new EventEmitter<void>();
-
-  onClick(): void {
-    this.click.emit();
-  }
+  @Input({ required: false }) label!: string;
 }
