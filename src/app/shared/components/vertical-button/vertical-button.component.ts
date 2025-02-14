d690b36ca2b74239d0f385540de0c66d83dfa901
devcobos <devcobos@gmail.com>
2025-02-14 21:11:40 +0100

    better semantic

--- src/app/shared/components/vertical-button/vertical-button.component.ts
+++ src/app/shared/components/vertical-button/vertical-button.component.ts
@@ -2,11 +2,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { NoSelectTextDirective } from '@shared/directives/no-select-text.directive';
 
 @Component({
   selector: 'vertical-button',
-  imports: [MatButtonModule, MatIconModule, NoSelectTextDirective, MatRippleModule],
+  imports: [MatButtonModule, MatIconModule, MatRippleModule],
   templateUrl: './vertical-button.component.html',
   styleUrl: './vertical-button.component.scss',
 })
@@ -16,7 +15,7 @@ export class VerticalButtonComponent {
 
   @Output() click = new EventEmitter<void>();
 
-  onDivClick(): void {
+  onClick(): void {
     this.click.emit();
   }
 }
