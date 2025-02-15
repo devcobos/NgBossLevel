5163c92faa601236ddf19274bad3a74d81570a5d
devcobos <devcobos@gmail.com>
2025-02-15 12:14:43 +0100

    Updated package.json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, provideRouter, withPreloading } from '@angular/route
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { InMemoryCache } from '@apollo/client/core';
-import { graphqlProvider } from '@config/apollo.config';
 import { provideMatIconCustomConfig } from '@config/icon-registry.config';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
@@ -17,7 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
     provideMatIconCustomConfig(),
-    graphqlProvider,
     provideApollo(() => {
       const httpLink = inject(HttpLink);
 
